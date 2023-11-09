edb546201c88c97926161ead3d5759b1da2239a7
Vaibhav Dabhi <pci100@server7.com>
2023-11-09 17:09:20 +0530

    initial commit

---
+++ src/main/java/com/example/githubaction/Controller.java
@@ -0,0 +1,13 @@
+package com.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String homePage() {
+		return "Welcome to first github action demo";
+	}
+}
